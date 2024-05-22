824efebcaa4b57812da0c98fdec0cf7b5eb259bb
_scss <106840557+user-non3@users.noreply.github.com>
2024-05-22 18:16:47 +0500

    Update config.py

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ class Color():
     custom = 0x00ca60
 
 class Settings():
-    token_bot = 'MTIyMzkzMzc4NTAyNDg5MjkzOQ.Gx28qe.s_3qFqG54wIMWTYYFy4YXnKS8OflFjrBTmb37E'
-    token_test = 'MTAwNTgyOTQ4MDQyMzYyODgyMg.Gt539t.tDjs9NhH1Zm1MqrNH5HpM7qOhVWncskuaQc1Us'
-    token_gerstlix = 'odmM64yLoAWyzXE5VrKnBqwmaafscNqE'
+    token_bot = 'MTIyMzkzMzc4NTAyNDg5MjkzOQ.Gx28qe.'
+    token_test = 'MTAwNTgyOTQ4MDQyMzYyODgyMg.Gt539t.'
+    token_gerstlix = 'token'
     token_api = 'eJgoei1lbppZuCXQbnocOdwAj'
-    token_vprikol = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MzdmNjcxYWFhMDk2YmRjNmNhMWM3ZCIsIm1ldGhvZHMiOlsibWVtYmVycyIsInJhdGluZyIsImZpbmQiLCJzdGF0dXMiLCJnaGV0dG8iLCJjaGVja3JwIiwicnBuaWNrIiwiZm9ydW0iLCJwbGF5ZXJzIiwib25saW5lIl0sImFudGlmbG9vZF9ieXBhc3MiOmZhbHNlLCJhZG1pbl9hY2Nlc3MiOmZhbHNlLCJleHAiOjI0NzE4MDg1OTl9.2Hd90-AAGBWbXnrpAIf3UPieqY-kdCsSoHT5r2yqyoY'
+    token_vprikol = 'token'
